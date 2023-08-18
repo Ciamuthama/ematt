78dc110047c01b11c325b24f9e8f2310d3c97278
Ciamuthama <muthamapeter79@gmail.com>
2023-08-18 16:41:19 +0300

    Added `minuscircleo` and `pluscircleo` icons to the product card. To handle the addition of products

--- screens/productCard.js
+++ screens/productCard.js
@@ -14,7 +14,7 @@ export default function ProductCard({
     const [count, setCount] = useState(1);
 
     return (
-        <View className="w-full bg-white dark:bg-gray-500 rounded-3xl p-5 my-5 ">
+        <View className="w-full bg-white dark:bg-gray-50/10 rounded-3xl p-5 my-5 ">
             <TouchableOpacity className='bg-white rounded-xl'>
                 <Image
                     source={{ uri: image }}
@@ -23,8 +23,23 @@ export default function ProductCard({
                 />
             </TouchableOpacity>
             <View className='mt-5'>
-                <Text>{category}</Text>
-                <Text>{title}</Text>
+                <Text className='text-sm text-black/60'>{category}</Text>
+                <Text className='text-lg font-semibold dark:text-white'>{title}</Text>
+                <View className='flex-row items-center gap-3'>
+                    <AntDesign
+                        name="minuscircleo"
+                        color={colorScheme === 'light' ? 'black' : 'white'}
+                        size={24}
+                        onPress={() => setCount(count - 1)}
+                    />
+                    <Text className='text-xl dark:text-white'>{count}</Text> 
+                    <AntDesign
+                        name="pluscircleo"
+                        color={colorScheme === 'light' ? 'black' : 'white'}
+                        size={24}
+                        onPress={() => setCount(count + 1)}
+                    />
+                </View>
                 <Text>{description}</Text>
                 <Text>{price}</Text>
             </View>
